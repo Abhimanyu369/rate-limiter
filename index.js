187ec08e56ef6d16d88c8841bd1e448d34da1853
Abhimanyu Malik <abhimanyumalik369@gmail.com>
2021-10-14 17:22:23 +0530

    Rate-limiter update for multiple api

--- index.js
+++ index.js
@@ -3,20 +3,38 @@ const express = require('express');
 const rateLimit = require('express-rate-limit');
 const app = express();
 
+// API 1
 const limiter = rateLimit({
-  windowMs: process.env.API_RATE_LIMIT_WINDOW * 60 * 1000, // 15 minutes
-  max: process.env.API_RATE_LIMIT, // limit each IP to 100 requests per windowMs
+  windowMs: process.env.API_RATE_LIMIT_WINDOW_1 * 60 * 1000, // 15 minutes
+  max: process.env.API_RATE_LIMIT_1, // limit each IP to 100 requests per windowMs
   message:
     'Too many accounts created from this IP, please try again after an hour',
 });
 
-const checkLogin = (req, res, next) => {
-  console.log('Login Logic');
+const checkLogic1 = (req, res, next) => {
+  console.log('Login Logic 1');
   next();
 };
 
-app.get('/api/', checkLogin, limiter, function (req, res) {
+app.get('/api/', checkLogic1, limiter, function (req, res) {
   return res.send('API WORKS!');
+});
+
+// API 2
+const limiterOther = rateLimit({
+  windowMs: process.env.API_RATE_LIMIT_WINDOW_2 * 60 * 1000, // 15 minutes
+  max: process.env.API_RATE_LIMIT_2, // limit each IP to 100 requests per windowMs
+  message:
+    'Too many accounts created from this IP, please try again after an hour',
+});
+
+const checkLogic2 = (req, res, next) => {
+  console.log('Login Logic 2');
+  next();
+};
+
+app.get('/api/other', checkLogic2, limiterOther, function (req, res) {
+  return res.send('API WORKS FOR OTHER!');
 });
 
 // Port
