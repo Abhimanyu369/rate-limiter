b7a57e3a48670d1345a139339c48cb612f1dd957
Abhimanyu Malik <abhimanyumalik369@gmail.com>
2021-10-14 18:21:23 +0530

    Limiter message update

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const app = express();
 const limiter = rateLimit({
   windowMs: process.env.API_RATE_LIMIT_WINDOW_1 * 60 * 1000, // 15 minutes
   max: process.env.API_RATE_LIMIT_1, // limit each IP to 100 requests per windowMs
-  message:
-    'Too many accounts created from this IP, please try again after an hour',
+  message: 'Too many API hits from this IP, please try again after an hour',
 });
 
 const checkLogic1 = (req, res, next) => {
@@ -24,8 +23,7 @@ app.get('/api/', checkLogic1, limiter, function (req, res) {
 const limiterOther = rateLimit({
   windowMs: process.env.API_RATE_LIMIT_WINDOW_2 * 60 * 1000, // 15 minutes
   max: process.env.API_RATE_LIMIT_2, // limit each IP to 100 requests per windowMs
-  message:
-    'Too many accounts created from this IP, please try again after an hour',
+  message: 'Too many API hits from this IP, please try again after an hour',
 });
 
 const checkLogic2 = (req, res, next) => {
